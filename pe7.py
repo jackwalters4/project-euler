490a9377156c1ce123cf6773436bd5cbf014a2a8
Jack Walters <jackwalters@Jacks-MacBook-Pro.local>
2019-12-30 14:21:25 -0500

    implementation of 7

---
+++ pe7.py
@@ -0,0 +1,26 @@
+#Jack Walters
+#Solution to Project Euler 7
+#What is the 10001st prime number?
+import math
+
+def is_prime(num) :
+	i = int(math.sqrt(num))
+	while (i > 1) :
+		if (num % i == 0) :
+			return False
+		i -= 1
+	return True
+
+
+def prime_numbers() :
+	i = 2
+	primes = []
+	while (len(primes) < 10002) :
+		if (is_prime(i)) :
+			primes.append(i)
+		i += 1
+	return primes
+
+
+primes = prime_numbers()
+print(primes[10001])
